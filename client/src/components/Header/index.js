fdf491dccf6260c06b997f635cbc819ae887df50
Catalystix <dcode.green@gmail.com>
2023-05-04 20:11:40 -0400

    update for localhost

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -13,9 +13,9 @@ const Header = () => {
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Articles</h1>
+            <h1 className="m-0">Images and Articles</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
+          <p className="m-0">Check out this shit</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
