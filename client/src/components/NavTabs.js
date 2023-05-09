9943982717e8df347449fcdf95b5aad3f6d4b756
Alex Barrett <akbarrett1993@gmail.com>
2023-05-08 20:47:46 -0400

    container and page rearrangement

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -3,42 +3,46 @@ import { Menu } from 'semantic-ui-react';
 
 export default function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <div style={{ backgroundColor: '#2d283e' }}>
-      <Menu pointing secondary  size="large">
+    <div style={{ backgroundColor: "#2d283e" }}>
+      <Menu pointing secondary size="large">
         <Menu.Item
-          name='Pic of the Day'
-          active={currentPage === 'APODsection'}
-          onClick={() => handlePageChange('APODsection')}
+          name="Pic of the Day"
+          active={currentPage === "APODsection"}
+          onClick={() => handlePageChange("APODsection")}
           style={{
-            color: currentPage === 'APODsection' ? '#d1d7e0' : '#564f6f',
-            borderBottom: currentPage === 'APODsection' ? '2px solid #d1d7e0' : '',
+            color: currentPage === "APODsection" ? "#d1d7e0" : "#564f6f",
+            borderBottom:
+              currentPage === "APODsection" ? "2px solid #d1d7e0" : "",
           }}
         />
         <Menu.Item
-          name='Mars Photos'
-          active={currentPage === 'MarsDisplay'}
-          onClick={() => handlePageChange('ProjecMarsDisplayts')}
+          name="Mars Photos"
+          active={currentPage === "MarsDisplay"}
+          onClick={() => handlePageChange("ProjecMarsDisplayts")}
           style={{
-            color: currentPage === 'MarsDisplay' ? '#d1d7e0' : '#564f6f',
-            borderBottom: currentPage === 'MarsDisplay' ? '2px solid #d1d7e0' : '',
+            color: currentPage === "MarsDisplay" ? "#d1d7e0" : "#564f6f",
+            borderBottom:
+              currentPage === "MarsDisplay" ? "2px solid #d1d7e0" : "",
           }}
         />
         <Menu.Item
-          name='Space Articles'
-          active={currentPage === 'TechArticle'}
-          onClick={() => handlePageChange('TechArticle')}
+          name="Space Articles"
+          active={currentPage === "TechArticle"}
+          onClick={() => handlePageChange("TechArticle")}
           style={{
-            color: currentPage === 'TechArticle' ? '#d1d7e0' : '#564f6f',
-            borderBottom: currentPage === 'TechArticle' ? '2px solid #d1d7e0' : '',
+            color: currentPage === "TechArticle" ? "#d1d7e0" : "#564f6f",
+            borderBottom:
+              currentPage === "TechArticle" ? "2px solid #d1d7e0" : "",
           }}
         />
         <Menu.Item
-          name='Resume'
-          active={currentPage === 'Resume'}
-          onClick={() => handlePageChange('Resume')}
+          name="Favorites"
+          active={currentPage === "Favorites"}
+          onClick={() => handlePageChange("Favorites")}
           style={{
-            color: currentPage === 'Resume' ? '#d1d7e0' : '#564f6f',
-            borderBottom: currentPage === 'Resume' ? '2px solid #d1d7e0' : '',
+            color: currentPage === "Favorites" ? "#d1d7e0" : "#564f6f",
+            borderBottom:
+              currentPage === "Favorites" ? "2px solid #d1d7e0" : "",
           }}
         />
       </Menu>
