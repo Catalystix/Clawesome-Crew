ac7ab708c6c74bacd3ac3ea0be06e1657a9379bf
Bronson Wardle <bronson.wardle@gmail.com>
2023-05-09 18:23:53 -0400

    created save button for articles but its not fully working

--- client/src/components/pages/TechSingle.js
+++ client/src/components/pages/TechSingle.js
@@ -6,78 +6,99 @@ import { ADD_IMAGE, ADD_ARTICLE } from "../../utils/mutations";
 import { Segment, Grid, Image } from "semantic-ui-react";
 
 const styles = {
-  img: {
-    maxHeight: "200px",
-    maxWidth: "200px",
-  },
-  overflow: {
-    whiteSpace: "nowrap",
-    width: "700px",
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-  },
+	img: {
+		maxHeight: "200px",
+		maxWidth: "200px",
+	},
+	overflow: {
+		whiteSpace: "nowrap",
+		width: "700px",
+		overflow: "hidden",
+		textOverflow: "ellipsis",
+	},
 };
 
 const TechDisplay = () => {
-  const [addArticle, { err }] = useMutation(ADD_ARTICLE);
-  const [articles, setArticles] = useState([]);
-  useEffect(async () => {
-    const data = await techCall();
-    console.log("techapi", data);
-    // const results = data.results.map(result => {
-    //   return {img: result[10], id: result[0], description: result[3]}
-    // })
-    // console.log(results);
-    setArticles(data.results);
-  }, []);
+	const [addArticle, { err }] = useMutation(ADD_ARTICLE);
+	const [articles, setArticles] = useState([]);
+	useEffect(async () => {
+		const data = await techCall();
+		console.log("techapi", data);
+		// const results = data.results.map(result => {
+		//   return {img: result[10], id: result[0], description: result[3]}
+		// })
+		// console.log(results);
+		setArticles(data.results);
+	}, []);
 
-  async function saveArticles(article) {
-    console.log("article", article);
-    const { data } = await addArticle({
-      variables: article,
-    });
-  }
-  console.log(articles);
+	async function saveArticles(article) {
+		console.log("article", article);
+		const { data } = await addArticle({
+			variables: article,
+		});
+	}
+	console.log(articles);
 
-  // const article_ID = localStorage.getItem("article_ID");
-  // const article_img = localStorage.getItem("article_img");
-  // const article_description = localStorage.getItem("article_description");
-  return (
-    <Segment>
-      <div style={styles.overflow}>
-        <h2>Tech Article of the Day</h2>
-        {/* <a href={article_ID} target="_blank" rel="noreferrer">
+	// const article_ID = localStorage.getItem("article_ID");
+	// const article_img = localStorage.getItem("article_img");
+	// const article_description = localStorage.getItem("article_description");
+	return (
+		<Segment>
+			<div style={styles.overflow}>
+				<h2>Tech Article of the Day</h2>
+				{/* <a href={article_ID} target="_blank" rel="noreferrer">
           <img style={styles.img} src={article_img} alt="tech stuff"></img>
           {article_description}
         </a> */}
-        <div className="toggleButton">
-          <button
-            onClick={async () => {
-              const data = await techCall();
-              console.log("data", data);
-              setArticles(data.results);
-            }}
-          >
-            Tech Search
-          </button>
-          <div>
-            {articles.map((article) => (
-              <a href={article[0]} target="_blank" rel="noreferrer">
-                <div key={article[0]}>
-                  <img
-                    style={styles.img}
-                    src={article[10]}
-                    alt="tech stuff"
-                  ></img>
-                  {article[3]}
-                </div>
-              </a>
-            ))}
-          </div>
-        </div>
-      </div>
-    </Segment>
-  );
+				<div className="toggleButton">
+					<button
+						onClick={async () => {
+							const data = await techCall();
+							console.log("data", data);
+							setArticles(data.results);
+						}}
+					>
+						Tech Search
+					</button>
+					<div>
+						{articles.map((article) => (
+							<a href={article[0]} target="_blank" rel="noreferrer">
+								<div key={article[0]}>
+									<img
+										style={styles.img}
+										src={article[10]}
+										alt="tech stuff"
+									></img>
+									<p>description: {article[3]}</p>
+								</div>
+								<button
+									data-url={article[0]}
+									data-img={article[10]}
+									data-description={article[3]}
+									onClick={(e) => {
+										e.preventDefault();
+										saveArticles({
+											url: e.target.dataset.url,
+											img: e.target.dataset.img,
+											description: e.target.dataset.description,
+										});
+									}}
+								>
+									Save
+								</button>
+							</a>
+						))}
+					</div>
+				</div>
+			</div>
+		</Segment>
+	);
 };
 
 export default TechDisplay;
+
+// <button onClick={(e) => saveArticles({
+//   url: e.target.dataset.article[0],
+//   img: e.target.dataset.article[10],
+//   description: e.target.dataset.article[3],
+// })}>Save</button>
