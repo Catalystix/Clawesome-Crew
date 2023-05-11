7ea8beab4c73325395d24a7528ee0955502960e8
erinsawyer504 <erinsawyer504@gmail.com>
2023-05-11 18:48:33 -0400

    Changed POD

--- client/src/components/pages/APODSingle.js
+++ client/src/components/pages/APODSingle.js
@@ -52,7 +52,7 @@ const ApodDisplay = () => {
           <Divider />
 
           <div style={{ textAlign: "center" }}>
-            <Image src={pod?.hdurl} size="big" centered raised></Image>
+            <Image src={pod?.hdurl} size="big" centered raised bordered></Image>
             <Divider />
             <Button
               color="red"
