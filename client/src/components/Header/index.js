79201a3f2b7332171a4296b7225d1aaa6bbd9a41
erinsawyer504 <erinsawyer504@gmail.com>
2023-05-11 19:20:49 -0400

    styled buttons on header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Segment, Grid, Image } from 'semantic-ui-react';
+import { Segment, Grid, Image, Button } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import logoImg from "../../assets/catlogo.png";
 
@@ -15,7 +15,7 @@ const Header = () => {
     <Grid columns={2} padded style={{ backgroundColor: "#45a29e" }}>
       <Grid.Column>
         <Link to="/">
-          <img
+          <Image
             src={logoImg}
             width="100"
             length="100"
@@ -23,26 +23,32 @@ const Header = () => {
           />
         </Link>
       </Grid.Column>
-      <Grid.Column verticalAlign="middle">
-        {Auth.loggedIn() ? (
-          <>
-            <Link className="btn btn-lg btn-info m-2" to="/me">
-              {Auth.getProfile().data.username}'s profile
-            </Link>
-            <button className="btn btn-lg btn-light m-2" onClick={logout}>
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <Link className="btn btn-lg btn-info m-2" to="/login">
-              <button>Login</button>
-            </Link>
-            <Link className="btn btn-lg btn-light m-2" to="/signup">
-              <button>Signup</button>
-            </Link>
-          </>
-        )}
+      <Grid.Column verticalAlign="right" textAlign="middle">
+        <Grid.Row>
+        </Grid.Row>
+        <Grid.Row>
+          {Auth.loggedIn() ? (
+            <>
+              <Link className="btn btn-lg btn-info m-2" to="/me">
+                {Auth.getProfile().data.username}'s profile
+              </Link>
+              <Button  style={{ background: "#1f2833", color: "white" }} className="btn btn-lg btn-light m-2" onClick={logout}>
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Link className="btn btn-lg btn-info m-2" to="/login">
+                <Button style={{ background: "#1f2833", color: "white" }}>Login</Button>
+              </Link>
+              <Link className="btn btn-lg btn-light m-2" to="/signup">
+                <Button style={{ background: "#1f2833", color: "white" }}>Signup</Button>
+              </Link>
+            </>
+          )}
+        </Grid.Row>
+        <Grid.Row>
+        </Grid.Row>
       </Grid.Column>
     </Grid>
   );
