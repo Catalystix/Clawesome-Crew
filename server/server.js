78e5a942556dc7cad82a3bc0bd45d8a6a2466059
hopemansfield <hopemansfield@gmail.com>
2023-05-12 17:51:17 -0400

    and another thing

--- server/server.js
+++ server/server.js
@@ -82,7 +82,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
   db.once("open", () => {
     app.listen(PORT, () => {
-      console.log(`API server running on port 3000!`);
+      console.log(`API server running on port ${PORT}!`);
       console.log(
         `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
       );
