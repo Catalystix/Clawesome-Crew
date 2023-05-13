bd1333bfacbd8ba76dfe0e52a0b2cebe7b030269
erinsawyer504 <erinsawyer504@gmail.com>
2023-05-13 13:07:25 -0400

    Styled favorites to a card format

--- client/src/components/pages/Favorites.js
+++ client/src/components/pages/Favorites.js
@@ -2,22 +2,14 @@ import React from "react";
 import { useQuery, gql } from '@apollo/client';
 import { QUERY_ME } from "../../utils/queries";
 
-import { Segment, Grid, Image } from "semantic-ui-react";
+import { Segment, Grid, Image, Card } from "semantic-ui-react";
 import 'semantic-ui-css/semantic.min.css';
-//import { Article } from "../../../../server/models";
-
 
 export default function Favorites() {
     const {loading, data} = useQuery(QUERY_ME);
     const user = data?.me || []
     console.log(user, 'favorites display user');
-     
-     
-    //   if (error) return <p>Error :</p>;
-  
-    //   return <img src={data.image.url} />;
-    
-  
+
     return (
       <>
       {loading ? (
@@ -28,18 +20,16 @@ export default function Favorites() {
             <div className="ui padded segment" style={{ backgroundColor: "#1f2833", textAlign: "center" }}> 
                 <h2>Favorites</h2>
             </div>
-            {user?.images.map((image)=>(
-              <Image key={image._id} src={image.url} />
-            ))}
-            {}
-
-            
+            <Card.Group itemsPerRow={5}>
+              {user?.images.map((image)=>(
+                <Card key={image._id}>
+                  <Image src={image.url} />
+                </Card>
+              ))}
+            </Card.Group>
         </Segment>
       </div>
       )}
       </>
     );
-  
-    }
-
-
+}
