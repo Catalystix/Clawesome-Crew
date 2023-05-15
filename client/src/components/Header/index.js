e6e14f8e3e2987eb5a683f9dd295a8ab188a80af
Alex Barrett <akbarrett1993@gmail.com>
2023-05-15 11:29:09 -0400

    logout takes you to home page

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 import { Segment, Grid, Image, Button } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
@@ -8,7 +8,7 @@ import logoImg from "../../assets/catlogo.png";
 import Auth from "../../utils/auth";
 
 const Header = () => {
-  const [currentPage, setCurrentPage] = useState('');
+  const [currentPage, setCurrentPage] = useState("");
   const location = useLocation();
 
   const logout = (event) => {
@@ -23,110 +23,129 @@ const Header = () => {
 
   return (
     <div style={{ backgroundColor: "#v" }}>
-    <Segment basic>
-    <div className="ui padded segment" style={{ backgroundColor: "#45a29e" }}>
-    <Grid columns={2} padded>
-      <Grid.Column>
-        <Link to="/">
-          <Image
-            src={logoImg}
-            width="100"
-            length="100"
-            className="left floated"
-          />
-        </Link>
-        <h1>The Daily Clawsmic</h1>
-        {Auth.loggedIn() ? (
-          <>
-            <Menu pointing secondary size="large" style={{ border: "none"}}>
-              <Menu.Item
-                as={Link}
-                to="/APOD"
-                name="Pic of the Day"
-                style={{
-                  color: currentPage === '/APOD' ? "#66fcf1" : '#564f6f',
-                  borderBottom: currentPage === '/APOD' ? '2px solid #66fcf1' : '',
-                }}
-              />
-              <Menu.Item
-                name="Mars Photos"
-                as={Link}
-                to="/mars"
-                style={{
-                  color: currentPage === '/mars' ? "#66fcf1" : '#564f6f',
-                  borderBottom: currentPage === '/mars' ? '2px solid #66fcf1' : '',
-                }}
-              />
-              <Menu.Item
-                name="Space Articles"
-                as={Link}
-                to="/tech"
-                style={{
-                  color: currentPage === '/tech' ? "#66fcf1" : '#564f6f',
-                  borderBottom: currentPage === '/tech' ? '2px solid #66fcf1' : '',
-                }}
-              />
-              <Menu.Item
-                name="Favorites"
-                as={Link}
-                to="/favorites"
-                style={{
-                  color: currentPage === '/favorites' ? "#66fcf1" : '#564f6f',
-                  borderBottom: currentPage === '/favorites' ? '2px solid #66fcf1' : '',
-                }}
-              />
-            </Menu>
-          </>
-        ) : (
-          <>
-            <div></div>
-          </>
-        )}
-      </Grid.Column>
-      <Grid.Column verticalAlign="right" textAlign="middle">
-        <Grid.Row></Grid.Row>
-        <Grid.Row style={{ }}>
-          {Auth.loggedIn() ? (
-            <>
-              {/* <Link className="btn btn-lg btn-info m-2" to="/me" style={{ color: "white"}} >
+      <Segment basic>
+        <div
+          className="ui padded segment"
+          style={{ backgroundColor: "#45a29e" }}
+        >
+          <Grid columns={2} padded>
+            <Grid.Column>
+              <Link to="/">
+                <Image
+                  src={logoImg}
+                  width="100"
+                  length="100"
+                  className="left floated"
+                />
+              </Link>
+              <h1>The Daily Clawsmic</h1>
+              {Auth.loggedIn() ? (
+                <>
+                  <Menu
+                    pointing
+                    secondary
+                    size="large"
+                    style={{ border: "none" }}
+                  >
+                    <Menu.Item
+                      as={Link}
+                      to="/APOD"
+                      name="Pic of the Day"
+                      style={{
+                        color: currentPage === "/APOD" ? "#66fcf1" : "#564f6f",
+                        borderBottom:
+                          currentPage === "/APOD" ? "2px solid #66fcf1" : "",
+                      }}
+                    />
+                    <Menu.Item
+                      name="Mars Photos"
+                      as={Link}
+                      to="/mars"
+                      style={{
+                        color: currentPage === "/mars" ? "#66fcf1" : "#564f6f",
+                        borderBottom:
+                          currentPage === "/mars" ? "2px solid #66fcf1" : "",
+                      }}
+                    />
+                    <Menu.Item
+                      name="Space Articles"
+                      as={Link}
+                      to="/tech"
+                      style={{
+                        color: currentPage === "/tech" ? "#66fcf1" : "#564f6f",
+                        borderBottom:
+                          currentPage === "/tech" ? "2px solid #66fcf1" : "",
+                      }}
+                    />
+                    <Menu.Item
+                      name="Favorites"
+                      as={Link}
+                      to="/favorites"
+                      style={{
+                        color:
+                          currentPage === "/favorites" ? "#66fcf1" : "#564f6f",
+                        borderBottom:
+                          currentPage === "/favorites"
+                            ? "2px solid #66fcf1"
+                            : "",
+                      }}
+                    />
+                  </Menu>
+                </>
+              ) : (
+                <>
+                  <div></div>
+                </>
+              )}
+            </Grid.Column>
+            <Grid.Column verticalAlign="right" textAlign="middle">
+              <Grid.Row></Grid.Row>
+              <Grid.Row style={{}}>
+                {Auth.loggedIn() ? (
+                  <>
+                    {/* <Link className="btn btn-lg btn-info m-2" to="/me" style={{ color: "white"}} >
                 {Auth.getProfile().data.username}'s profile  
               </Link> */}
-              <Button 
-                  as={Link}
-                  to="/me"
-                  style={{ background: "#1f2833", color: "white"}}
-                >
-                  {Auth.getProfile().data.username}'s profile
-                </Button>
-              <Button
-                style={{ background: "#1f2833", color: "white", marginLeft: "1em" }}
-                className="btn btn-lg btn-light m-2"
-                onClick={logout}
-              >
-                Logout
-              </Button>
-            </>
-          ) : (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
-                <Button style={{ background: "#1f2833", color: "white" }}>
-                  Login
-                </Button>
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
-                <Button style={{ background: "#1f2833", color: "white" }}>
-                  Signup
-                </Button>
-              </Link>
-            </>
-          )}
-        </Grid.Row>
-        <Grid.Row></Grid.Row>
-      </Grid.Column>
-    </Grid>
+                    <Button
+                      as={Link}
+                      to="/me"
+                      style={{ background: "#1f2833", color: "white" }}
+                    >
+                      {Auth.getProfile().data.username}'s profile
+                    </Button>
+                    <Button
+                      style={{
+                        background: "#1f2833",
+                        color: "white",
+                        marginLeft: "1em",
+                      }}
+                      className="btn btn-lg btn-light m-2"
+                      onClick={logout}
+                    >
+                      Logout
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <Link className="btn btn-lg btn-info m-2" to="/login">
+                      <Button style={{ background: "#1f2833", color: "white" }}>
+                        Login
+                      </Button>
+                    </Link>
+                    <Link className="btn btn-lg btn-light m-2" to="/signup">
+                      <Button style={{ background: "#1f2833", color: "white" }}>
+                        Signup
+                      </Button>
+                    </Link>
+                  </>
+                )}
+              </Grid.Row>
+              <Grid.Row></Grid.Row>
+            </Grid.Column>
+          </Grid>
+        </div>
+      </Segment>
     </div>
-    </Segment>
-  </div>
   );
 };
 
